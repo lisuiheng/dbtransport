f47737e0bc275b30275e8633e4483b69584061fc
lisuiheng <lisuiheng1@gmail.com>
2017-06-19 13:59:35 +0800

    change pom

--- src/test/java/com/lisuiheng/ApplicationTest.java
+++ src/test/java/com/lisuiheng/ApplicationTest.java
@@ -1,25 +1,23 @@
-package com.lisuiheng;
-
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.*;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
-public class ApplicationTest {
-    @Autowired
-    private Application.DBTransport dbTransport;
-
-    @Test
-    public void transport() throws Exception {
-        dbTransport.transport();
-    }
-
-
-
-}
+//package com.lisuiheng;
+//
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import static org.junit.Assert.*;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = Application.class)
+//public class ApplicationTest {
+//    @Autowired
+//    private Application.DBTransport dbTransport;
+//
+//    @Test
+//    public void transport() throws Exception {
+//        dbTransport.transport();
+//    }
+//
+//}
